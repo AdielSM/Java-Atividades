95f99a6fd829178eaa9f0d5050bb7f0e116e34d6
AdielSM <129189553+AdielSM@users.noreply.github.com>
2024-01-03 17:28:07 -0300

    Primeiro Exercício

---
+++ src/Exercicios/Basico/exerc1.java
@@ -0,0 +1,14 @@
+package Exercicios.Basico;
+
+/*
+1. Write a Java program to print 'Hello' on screen and your name on a separate line.
+Expected Output :
+Hello
+Alexandra Abramov
+*/
+
+public class exerc1 {
+    public static void main(String[] args) {
+        System.out.println("Hello\nAdiel Melo");
+    }
+}
---
+++ src/Exercicios/Basico/exerc2.java
@@ -0,0 +1,4 @@
+package Exercicios.Basico;
+
+public class exerc2 {
+}
