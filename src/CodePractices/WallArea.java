24668b1b86e1337b6728284409f2a2c92bd68fca
AdielSM <129189553+AdielSM@users.noreply.github.com>
2024-01-22 16:07:56 -0300

    +3 codePratices

---
+++ src/CodePractices/WallArea.java
@@ -0,0 +1,64 @@
+package CodePractices;
+
+/*
+Write a class with the name Wall. The class needs two fields (instance variables) with name width and height of type double.
+
+
+
+The class needs to have two constructors:
+
+The first constructor does not have any parameters (no-args constructor).
+
+The second constructor has parameters width and height of type double and it needs to initialize the fields.
+
+In case the width parameter is less than 0 it needs to set the width field value to 0.
+
+In case the height parameter is less than 0 it needs to set the height field value to 0.
+
+
+
+Write the following methods (instance methods):
+
+Method named getWidth without any parameters, it needs to return the value of width field.
+
+Method named getHeight without any parameters, it needs to return the value of height field.
+
+Method named setWidth with one parameter of type double, it needs to set the value of the width field. If the parameter is less than 0 it needs to set the width field value to 0.
+
+Method named setHeight with one parameter of type double, it needs to set the value of the height field. If the parameter is less than 0 it needs to set the height field value to 0.
+
+Method named getArea without any parameters, it nee
+ */
+public class WallArea {
+    private double width;
+    private double height;
+
+    public WallArea() {
+        this(0, 0);
+    }
+
+    public WallArea(double width, double height) {
+        this.width = width < 0 ? 0 : width;
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setWidth(double width) {
+        this.width = width < 0 ? 0 : width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+}
