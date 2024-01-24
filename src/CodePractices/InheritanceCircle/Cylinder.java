62b3d927c5b0e0770786eba427287580672f805c
AdielSM <129189553+AdielSM@users.noreply.github.com>
2024-01-24 14:34:33 -0300

    2 codePratices de herença

---
+++ src/CodePractices/InheritanceCircle/Cylinder.java
@@ -0,0 +1,18 @@
+package CodePractices.InheritanceCircle;
+
+public class Cylinder extends Circle{
+    private double height;
+
+    public Cylinder(double radius, double height){
+        super(radius);
+        this.height = Math.max(0,height);
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public double getVolume(){
+        return super.getArea() * height;
+    }
+}
