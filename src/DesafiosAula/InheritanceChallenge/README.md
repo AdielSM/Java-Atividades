d0c0c74a4c760842404a2041385f720816052df3
AdielSM <129189553+AdielSM@users.noreply.github.com>
2024-01-25 15:50:23 -0300

    add README.md

---
+++ src/DesafiosAula/InheritanceChallenge/README.md
@@ -0,0 +1,6 @@
+## instructions
+1 - Create a class Worker with the following attributes: name, birthDate, endDate. Create the methods getAge, collectPay and terminate.
+
+2 - Create a class Employee that inherits from the Worker class and has the following attributes: employeeld, hireDate.
+
+3 - Create a class SalaryEmployee that inherits from the Employee class and has the following attributes: annualSalary, isRetired. Create the methods retire.
--- src/DesafiosAula/InheritanceChallenge/Worker.java
+++ src/DesafiosAula/InheritanceChallenge/Worker.java
@@ -1,13 +1,5 @@
 package DesafiosAula.InheritanceChallenge;
 
-/*
-1 - Create a class Worker with the following attributes: name, birthDate, endDate. Create the methods getAge, collectPay and terminate.
-
-2 - Create a class Employee that inherits from the Worker class and has the following attributes: employeeld, hireDate.
-
-3 - Create a class SalaryEmployee that inherits from the Employee class and has the following attributes: annualSalary, isRetired. Create the methods retire.
- */
-
 import java.time.Year;
 
 public class Worker {
